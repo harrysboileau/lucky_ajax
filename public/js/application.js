16bb30ae46834ca71b2991f175aaf5fb21f561ab
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-09 18:34:57 -0500

    final ajax

--- public/js/application.js
+++ public/js/application.js
@@ -1,8 +1,20 @@
 $(document).ready(function () {
+  $("#die_roll").click(function(event) { 
+    event.preventDefault(); console.log("click!")
+
+  var roll = Math.floor((Math.random()*6)+1);
+  console.log(roll);
+
+  var data = {value: roll}
+
+  $.post('/rolls', data, function(response){
+    $('#die').html(response);
+   });
+ });
+  // $("input type").click
 
   // PSEUDO-CODE:
-  //   1- intercept the form submission event using jQuery
-  //   2- prevent the default action for that event from happening
+
   //   3- generate a random number between 1 and 6 using JavaScript
   //   4- use jQuery to submit an AJAX post to the form's action
   //   5- when the AJAX post is done, replace the contents of the "#die" DIV in the DOM using jQuery
